1c66cc0c9fd2bbfc89739f3764f5827ef3c3913a
Pccoe <siddhant.mishriktokar22@pccoepuen.org>
2024-03-18 10:56:40 +0530

    Ass2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<WeatherReport> monthlyData = new ArrayList<>();
 
-// Sample data for January
+
         monthlyData.add(new WeatherReport(1, 25.0, 10.0, 0.5, 0.0));
         monthlyData.add(new WeatherReport(2, 28.0, 12.0, 0.2, 0.0));
-// Add more days as needed
+
 
         MonthlyReportGenerator generator = new MonthlyReportGenerator(monthlyData);
         generator.generateReport();
